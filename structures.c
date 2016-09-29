edf2596abbf38f6c91864032979de48ad03cf396
Jack Garrard <speyejack@gmail.com>
2016-09-29 00:53:21 -0700

    Gave IDs to strucs

--- structures.c
+++ structures.c
@@ -1,37 +1,27 @@
 typedef struct {
-  int id;
+  int id; // ID: 0
 } Object;
 
 typedef struct {
-  int id;
+  int id; // ID: 1
   double width;
   double height;
 } Camera;
 
 typedef struct {
-  int id;
+  int id;// ID: 2
   double* color;
   double* pos;
   double radius;
 } Sphere;
 
 typedef struct {
-  int id;
+  int id;// ID: 3
   double* color;
   double* pos;
   double* normal;
 } Plane;
 
 typedef struct {
-  int id;
-  double A;
-  double B;
-  double C;
-  double D;
-  double E;
-  double F;
-  double G;
-  double H;
-  double I;
-  double J;
+  int id;// ID: 4
 } Quadric;
