8649b6179d0110c9fa6ebc76e8bebe3447ea3ecf
Jack Garrard <speyejack@gmail.com>
2016-09-30 11:06:20 -0700

    Implemented personal data structure along with object rendering

--- raycaster.c
+++ raycaster.c
@@ -2,26 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include "ppmrw.c"
-// Plymorphism in C
-
-typedef struct {
-  int kind; // 0 = cylinder, 1 = sphere, 2 = teapot
-  double color[3];
-  union {
-    struct {
-      double center[3];
-      double radius;
-    } cylinder;
-    struct {
-      double center[3];
-      double radius;
-    } sphere;
-    struct {
-      double handle_length;
-    } teapot;
-  };
-} Object;
-
+#include "structures.c"
 
 static inline double sqr(double v) {
   return v*v;
@@ -35,64 +16,28 @@ static inline void normalize(double* v) {
   v[2] /= len;
 }
 
-
-double cylinder_intersection(double* Ro, double* Rd,
-			     double* C, double r) {
-  // Step 1. Find the equation for the object you are
-  // interested in..  (e.g., cylinder)
-  //
-  // x^2 + z^2 = r^2
-  //
-  // Step 2. Parameterize the equation with a center point
-  // if needed
-  //
-  // (x-Cx)^2 + (z-Cz)^2 = r^2
-  //
-  // Step 3. Substitute the eq for a ray into our object
-  // equation.
-  //
-  // (Rox + t*Rdx - Cx)^2 + (Roz + t*Rdz - Cz)^2 - r^2 = 0
-  //
-  // Step 4. Solve for t.
-  //
-  // Step 4a. Rewrite the equation (flatten).
-  //
-  // -r^2 +
-  // t^2 * Rdx^2 +
-  // t^2 * Rdz^2 +
-  // 2*t * Rox * Rdx -
-  // 2*t * Rdx * Cx +
-  // 2*t * Roz * Rdz -
-  // 2*t * Rdz * Cz +
-  // Rox^2 -
-  // 2*Rox*Cx +
-  // Cx^2 +
-  // Roz^2 -
-  // 2*Roz*Cz +
-  // Cz^2 = 0
-  //
-  // Steb 4b. Rewrite the equation in terms of t.
-  //
-  // t^2 * (Rdx^2 + Rdz^2) +
-  // t * (2 * (Rox * Rdx - Rdx * Cx + Roz * Rdz - Rdz * Cz)) +
-  // Rox^2 - 2*Rox*Cx + Cx^2 + Roz^2 - 2*Roz*Cz + Cz^2 - r^2 = 0
-  //
-  // Use the quadratic equation to solve for t..
-  double a = (sqr(Rd[0]) + sqr(Rd[2]));
-  double b = (2 * (Ro[0] * Rd[0] - Rd[0] * C[0] + Ro[2] * Rd[2] - Rd[2] * C[2]));
-  double c = sqr(Ro[0]) - 2*Ro[0]*C[0] + sqr(C[0]) + sqr(Ro[2]) - 2*Ro[2]*C[2] + sqr(C[2]) - sqr(r);
-
+int sphere_intersection(double* Ro, double* Rd, double* C, double r){
+  double a = sqr(Rd[0]) + sqr(Rd[1]) + sqr(Rd[2]);
+  double b =  2 * (Rd[0] * (Ro[0] - C[0]) + Rd[1] * (Ro[1] - C[1]) + Rd[2] * (Ro[2] - C[2]));
+  double c = sqr(Ro[0] - C[0]) + sqr(Ro[1] - C[1]) + sqr(Ro[2] - C[2]) - sqr(r);
   double det = sqr(b) - 4 * a * c;
   if (det < 0) return -1;
-
-  det = sqrt(det);
   
-  double t0 = (-b - det) / (2*a);
-  if (t0 > 0) return t0;
+  det = sqrt(det);
 
-  double t1 = (-b + det) / (2*a);
-  if (t1 > 0) return t1;
+  double t = (-b - det)/(2 * a);
+  if (t > 0) return t;
+  t = (-b + det)/(2 * a);
+  if (t > 0) return t;
 
+  return -1;
+}
+
+int plane_intersection(double* Ro, double* Rd, double* p, double* n){
+  // a(xr - x0) + b(yr - y0,) + c(zr - z0) = 0
+
+  double t = (n[0] * Ro[0]- n[0] * p[0] + n[1] * Ro[1] - n[1] * p[1] + n[2] * Ro[2] - n[2] * p[2]) / - (n[0] * Rd[0] + n[1] * Rd[1] + n[2] * Rd[2]);
+  if (t > 0) return t;
   return -1;
 }
 
@@ -100,17 +45,18 @@ Image* paint_scene() {
 
   Object** objects;
   objects = malloc(sizeof(Object*)*2);
-  objects[0] = malloc(sizeof(Object));
-  objects[0]->kind = 0;
-  objects[0]->color[0] = 0;
-  objects[0]->color[1] = 0;
-  objects[0]->color[2] = 255;
+  Sphere* sphere = malloc(sizeof(Sphere));
+  objects[0] = (Object*) sphere;
+  sphere->id = 2;
+  sphere->color[0] = 0;
+  sphere->color[1] = 0;
+  sphere->color[2] = 255;
   
-  objects[0]->cylinder.radius = 2;
+  sphere->radius = 2;
   // object[0]->teapot.handle_length = 2;
-  objects[0]->cylinder.center[0] = 0;
-  objects[0]->cylinder.center[1] = 0;
-  objects[0]->cylinder.center[2] = 20;
+  sphere->pos[0] = 0;
+  sphere->pos[1] = 0;
+  sphere->pos[2] = 20;
   objects[1] = NULL;
   
   double cx = 0;
@@ -118,9 +64,9 @@ Image* paint_scene() {
   double h = 0.7;
   double w = 0.7;
 
-  int M = 20;
-  int N = 20;
-
+  int M = 200;
+  int N = 200;
+  
   Image* img = malloc(sizeof(Image));
   img->width = N;
   img->height = M;
@@ -139,37 +85,41 @@ Image* paint_scene() {
 	1
       };
       normalize(Rd);
-
       double best_t = INFINITY;
       int best_i = -1;
       for (int i=0; objects[i] != 0; i += 1) {
 	double t = 0;
 
-	switch(objects[i]->kind) {
-	case 0:
-	  t = cylinder_intersection(Ro, Rd,
-				    objects[i]->cylinder.center,
-				    objects[i]->cylinder.radius);
+	switch(objects[i]->id) {
+	case 2 :
+	  ;
+	  Sphere* s = (Sphere*) objects[i];
+	  t = sphere_intersection(Ro, Rd,
+				    s->pos,
+				    s->radius);
 	  break;
+	case 3 :
+	  ;
+	  Plane* p = (Plane*) objects[i];
+	  t = plane_intersection(Ro, Rd,
+				    p->pos,
+				    p->normal);
 	default:
-	  // Horrible error
+	  fprintf(stderr, "Unsupported object during rendering\n");
 	  exit(1);
 	}
 	if (t > 0 && t < best_t){
 	  best_t = t;
-	  best_i = i;
-	}
+	  best_i = i;}
       }
-      if (best_t > 0 && best_t != INFINITY && (objects[best_i]->kind == 0 || objects[best_i]->kind == 1)) {
-	double* color = objects[best_i]->color;
+      if (best_t > 0 && best_t != INFINITY && (objects[best_i]->id == 2 || objects[best_i]->id == 3)) {
+  double* color = ((Sphere*)objects[best_i])->color;
 	Pixel pix;
 	pix.r = color[0];
 	pix.g = color[1];
 	pix.b = color[2];
-	printf("%d %d %d\n",color[0], color[1], color[2]);
 	img->buffer[y * N + x] = pix;
       } else {
-	double* color = objects[best_i]->color;
 	Pixel pix;
 	pix.r = 0;
 	pix.g = 0;
@@ -179,7 +129,6 @@ Image* paint_scene() {
       }
       
     }
-    printf("\n");
   }
   
   return img;
